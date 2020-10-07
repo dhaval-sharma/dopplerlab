70966bd7c9b3886b6c7302ef7c2dcc1a75e1a22d
Dhaval Sharma <dhaval.sharma@valuelabs.com>
2020-10-07 10:03:01 +0530

    Updates

---
+++ precache-manifest.d89fa7f0216e0aa74a657118ee66fdde.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b03d357c3c4d1fdec1958adbe6b6e2",
+    "url": "/dopplerlab/index.html"
+  },
+  {
+    "revision": "37f3106989ea908411c0",
+    "url": "/dopplerlab/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4d34d4fd7c2ec8926847",
+    "url": "/dopplerlab/static/css/main.9cdf33c2.chunk.css"
+  },
+  {
+    "revision": "37f3106989ea908411c0",
+    "url": "/dopplerlab/static/js/2.b47a1779.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dopplerlab/static/js/2.b47a1779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d34d4fd7c2ec8926847",
+    "url": "/dopplerlab/static/js/main.5b04baf1.chunk.js"
+  },
+  {
+    "revision": "a2546e665f35ffc22ece",
+    "url": "/dopplerlab/static/js/runtime-main.a44ebb31.js"
+  },
+  {
+    "revision": "6ed9bb01e86a68ce936942d72ab87c1c",
+    "url": "/dopplerlab/static/media/activity.6ed9bb01.svg"
+  },
+  {
+    "revision": "42b86989416d01683ba187f216ac0615",
+    "url": "/dopplerlab/static/media/bg.42b86989.jpg"
+  },
+  {
+    "revision": "8673a3f738f4f617464397974be12a41",
+    "url": "/dopplerlab/static/media/book.8673a3f7.png"
+  },
+  {
+    "revision": "c3e670aad7c16fb6232186af402940aa",
+    "url": "/dopplerlab/static/media/bookmark.c3e670aa.svg"
+  },
+  {
+    "revision": "ab5523b1087134bb36f44ad2af9fe41c",
+    "url": "/dopplerlab/static/media/example.ab5523b1.svg"
+  },
+  {
+    "revision": "66f33c8e0ab26891b18618a32f75b368",
+    "url": "/dopplerlab/static/media/example1.66f33c8e.png"
+  },
+  {
+    "revision": "4e8db76acc627a9a21b813bd857e532d",
+    "url": "/dopplerlab/static/media/example2.4e8db76a.png"
+  },
+  {
+    "revision": "08b56228a88fc92813b9875e21fd1709",
+    "url": "/dopplerlab/static/media/example3.08b56228.png"
+  },
+  {
+    "revision": "3550342031cb42afe5e9e25371c44801",
+    "url": "/dopplerlab/static/media/introduction.35503420.svg"
+  },
+  {
+    "revision": "115a301aa41d2e84f64fc7d423c952b8",
+    "url": "/dopplerlab/static/media/logo.115a301a.PNG"
+  },
+  {
+    "revision": "5849fc029df7c824844e885abdc1d8f5",
+    "url": "/dopplerlab/static/media/note.5849fc02.svg"
+  },
+  {
+    "revision": "eba6df9ef10c4f17c7d40f1d95a33989",
+    "url": "/dopplerlab/static/media/small-star.eba6df9e.png"
+  },
+  {
+    "revision": "d668df6d2fbfa9e1ae6416879fa2e311",
+    "url": "/dopplerlab/static/media/velocity.d668df6d.jpg"
+  }
+]);
